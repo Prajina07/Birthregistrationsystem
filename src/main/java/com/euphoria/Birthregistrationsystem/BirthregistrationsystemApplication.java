e721c2f4bc09777ad76d26f0c861c088caa7fd18
Prajina <jinuprazu@gmail.com>
2021-05-30 21:51:23 +0545

    Initil Commit

---
+++ src/main/java/com/euphoria/Birthregistrationsystem/BirthregistrationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.euphoria.Birthregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthregistrationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthregistrationsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/euphoria/Birthregistrationsystem/BirthregistrationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euphoria.Birthregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthregistrationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
